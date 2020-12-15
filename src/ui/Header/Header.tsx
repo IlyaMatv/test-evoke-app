70afe6cb2cdabbe374b1f58c80b9615fbdb67a05
Ilya Matv <arkashabi4@gmail.com>
2020-12-15 20:59:23 +0300

    fix css

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Header.sass'
 import logo from '../../assets/logo.png'
 import Navigation from './Navigation/Navigation'
+import { NavLink } from 'react-router-dom'
 
 const Header: React.FC = () => {
 
     return (
         <header className="header">
-            <img className="logo" src={logo} alt="logo" />
+            <NavLink to="/">
+                <img className="logo" src={logo} alt="logo" />
+            </NavLink>
             <Navigation />
         </header >
     )
